9985b476f48763a046eeb7a6c373b3801d5c435b
Kemal Amru Ramadhan <kemalamru@gmail.com>
2017-12-12 16:13:55 +0700

    Setup Dagger

--- app/src/main/java/com/kar/dotaroid/injection/builder/ActivityBuilder.java
+++ app/src/main/java/com/kar/dotaroid/injection/builder/ActivityBuilder.java
@@ -1,5 +1,16 @@
 package com.kar.dotaroid.injection.builder;
 
+import com.kar.dotaroid.ui.hero.hero_list.HeroListActivity;
+import com.kar.dotaroid.ui.hero.hero_list.HeroListViewModel;
+import com.kar.dotaroid.ui.hero.hero_profile.HeroProfileActivity;
+import com.kar.dotaroid.ui.hero.hero_profile.HeroProfileViewModel;
+import com.kar.dotaroid.ui.main.MainActivity;
+import com.kar.dotaroid.ui.main.MainActivityModule;
+import com.kar.dotaroid.ui.player.player_list.PlayerListActivity;
+import com.kar.dotaroid.ui.player.player_list.PlayerListViewModel;
+import com.kar.dotaroid.ui.player.player_profile.PlayerProfileActivity;
+import com.kar.dotaroid.ui.player.player_profile.PlayerProfileViewModel;
+
 import dagger.Module;
 import dagger.android.ContributesAndroidInjector;
 
@@ -9,6 +20,19 @@ import dagger.android.ContributesAndroidInjector;
 
 @Module
 public abstract class ActivityBuilder {
-    
-}
 
+    @ContributesAndroidInjector(modules = {MainActivityModule.class})
+    abstract MainActivity bindMainActivity();
+
+    @ContributesAndroidInjector(modules = {PlayerListViewModel.class})
+    abstract PlayerListActivity bindPlayerListActivity();
+
+    @ContributesAndroidInjector(modules = {PlayerProfileViewModel.class})
+    abstract PlayerProfileActivity bindPlayerProfileActivity();
+
+    @ContributesAndroidInjector(modules = {HeroListViewModel.class})
+    abstract HeroListActivity bindHeroListActivity();
+
+    @ContributesAndroidInjector(modules = {HeroProfileViewModel.class})
+    abstract HeroProfileActivity bindHeroProfileActivity();
+}
